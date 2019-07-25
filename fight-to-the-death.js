f5e0c884d0c3a74d7a4670906eed1b9095e92e37
ktvo68 <43115241+ktvo68@users.noreply.github.com>
2019-07-25 09:03:27 -0700

    remove comments meant to be private

--- fight-to-the-death.js
+++ fight-to-the-death.js
@@ -1,11 +1,3 @@
-// My aplology.  This took a bit longer then I planned.  Ran into bugs and
-// I forgot I was supposed to take my kids to their swimming classes
-// In this version I added the implementation of 'dodge', 'role by initiative' and file import
-// I implemented 'roll by initiative' based on the definition in Urban Dictionary.
-// I did not understand the initiative data and sample output related to initiative data.
-// I did not implement 'critical' because there is not enough informatino on it.
-// I also declare the tournament winner.
-
 
 const fighters = require('./fighters.js')
 let Tournament = require('./tournament.js')
